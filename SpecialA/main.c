733f09642c5ffaafd92d8f471f7bffb6e2b84334
Zhongyang Zheng <89175460+Joker1581@users.noreply.github.com>
2023-05-03 20:42:59 +0800

    23.5.3

---
+++ SpecialA/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+int cmpfunc(const void* a, const void* b){
+    return *(int*)a - *(int*)b;
+}
+
+int specialArray(int* nums, int numsSize){
+    int i;
+    qsort(nums, numsSize, sizeof(int), cmpfunc);
+    
+    for(i = 0; i < numsSize; i++){
+        if(nums[i] >= (numsSize-i))
+            if(i == 0)
+                return (numsSize-i) ;
+            else{
+                if(nums[i-1] < (numsSize-i))
+                    return (numsSize-i) ;
+            }
+    }
+    
+    return -1;
+}
