61128321c248a0e4cb043ca8cad90e4964cd9221
Zhongyang Zheng <89175460+Joker1581@users.noreply.github.com>
2023-05-27 20:16:16 +0800

    23.5.27

---
+++ TwoSum/main.py
@@ -0,0 +1,18 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
+        vals = {}
+        def rec(node):
+            if not node:
+                return False
+            if node.val in vals:
+                return True
+            vals[k - node.val] = True
+            return rec(node.left) or rec(node.right)
+        return rec(root)
+        
